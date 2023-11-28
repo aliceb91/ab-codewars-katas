b6a9b485444d1513c8a999b03b2ee727ae20d5dc
Alice Birkett <alicebirkett@alice-b-mbp.home>
2023-11-28 13:27:22 +0000

    completed capitalisation kata

--- src/main/kotlin/katas/AlternateCapitalization.kt
+++ src/main/kotlin/katas/AlternateCapitalization.kt
@@ -3,6 +3,19 @@ package katas
 class AlternateCapitalization {
 
     fun capitalize(text:String): List<String> {
-        return listOf("A", "a")
+        var index: Int = 0
+        var upperFirst: String = ""
+        var lowerFirst: String = ""
+        for (char in text) {
+            if (index % 2 == 0) {
+                upperFirst += char.uppercase()
+                lowerFirst += char
+            } else {
+                upperFirst += char
+                lowerFirst += char.uppercase()
+            }
+            index += 1
+        }
+        return listOf(upperFirst, lowerFirst)
     }
 }
--- src/test/kotlin/katas/AlternateCapitalizationTest.kt
+++ src/test/kotlin/katas/AlternateCapitalizationTest.kt
@@ -23,10 +23,45 @@ class AlternateCapitalizationTest {
     }
 
     @Test
+    fun `given b it returns a list of B, b`() {
+        val underTest = AlternateCapitalization()
+        val expected: List<String> = listOf("B", "b")
+        val result: List<String> = underTest.capitalize("b")
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given any letter, it returns a list of LETTER, letter`() {
+        val underTest = AlternateCapitalization()
+        val expected1: List<String> = listOf("C", "c")
+        val expected2: List<String> = listOf("D", "d")
+        val result1: List<String> = underTest.capitalize("c")
+        val result2: List<String> = underTest.capitalize("d")
+        assertEquals(expected1, result1)
+        assertEquals(expected2, result2)
+    }
+
+    @Test
     fun `given ab, it returns a list of Ab, aB`() {
         val underTest = AlternateCapitalization()
         val expected: List<String> = listOf("Ab", "aB")
         val result: List<String> = underTest.capitalize("ab")
         assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given abc, it returns a list of AbC, aBc`() {
+        val underTest = AlternateCapitalization()
+        val expected: List<String> = listOf("AbC", "aBc")
+        val result: List<String> = underTest.capitalize("abc")
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given the string of abracadabra it returns the correct capitalisation`() {
+        val underTest = AlternateCapitalization()
+        val expected: List<String> = listOf("AbRaCaDaBrA", "aBrAcAdAbRa")
+        val result: List<String> = underTest.capitalize("abracadabra")
+        assertEquals(expected,result)
     }
 }
