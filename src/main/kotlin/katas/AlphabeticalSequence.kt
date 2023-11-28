cdadac009febd938f63c5b414121a2deb5c402f3
Alice Birkett <alicebirkett@alice-b-mbp.home>
2023-11-28 14:46:10 +0000

    Refactor following research

--- src/main/kotlin/katas/AlphabeticalSequence.kt
+++ src/main/kotlin/katas/AlphabeticalSequence.kt
@@ -5,16 +5,10 @@ class AlphabeticalSequence {
     fun alphaSeq(str: String): String {
         val lowerCaseString: String = str.lowercase()
         val alphabet: String = "abcdefghijklmnopqrstuvwxyz"
-        val stringList: List<String> = lowerCaseString.split("").drop(1).dropLast(1)
-        val sortedList: List<String> = stringList.sorted()
-        val finalList: List<String> = sortedList.map {
-            val charIndex = alphabet.indexOf(it)
-            var newStr = it.uppercase()
-            for (count in 1..charIndex) {
-                newStr += it
+        val charArr: CharArray = lowerCaseString.toCharArray()
+        val sortedList: List<Char> = charArr.sorted()
+        return sortedList.joinToString(",") {
+            "$it".uppercase() + "$it".repeat(it - 'a')
         }
-            newStr
-        }
-        return finalList.joinToString(",")
     }
 }
