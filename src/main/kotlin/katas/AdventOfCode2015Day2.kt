1c2fd2d90b1adcfff487569eedd93140200faadb
Alice Birkett <alicebirkett@alice-b-mbp.home>
2023-12-01 11:02:45 +0000

    Advent of code 2015 day 2 part 1 + middle character refactor

--- src/main/kotlin/katas/AdventOfCode2015Day2.kt
+++ src/main/kotlin/katas/AdventOfCode2015Day2.kt
@@ -2,7 +2,17 @@ package katas
 
 class AdventOfCode2015Day2 {
 
-    fun wrappingPaperCalc() {
-        TODO()
+    fun wrappingPaperCalc(str: String): Int {
+        var counter: Int = 0
+        val splitBoxes: List<String> = str.split("\n")
+        splitBoxes.forEach {
+            val measurement: List<String> = it.split("x")
+            val intMes: List<Int> = measurement.map {
+                it.toInt()
+            }
+            val small: Int = intMes.sorted()[0] * intMes.sorted()[1]
+            counter += 2 * intMes[0] * intMes[1] + 2 * intMes[1] * intMes[2] +2 * intMes[2] * intMes[0] + small
+        }
+        return counter
     }
 }
--- src/main/kotlin/katas/GetTheMiddleCharacter.kt
+++ src/main/kotlin/katas/GetTheMiddleCharacter.kt
@@ -5,11 +5,7 @@ import kotlin.math.ceil
 class GetTheMiddleCharacter {
 
     fun getMiddle(word: String): String {
-        if (word.length % 2 == 0) {
-            val midway: Int = word.length / 2
-            return word.substring(midway - 1..midway)
-        } else {
-            return word[(word.length / 2)].toString()
-        }
+
+        return word.substring((word.length - 1) / 2, word.length / 2 + 1)
     }
 }
