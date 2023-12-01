e8f132c4f868b837e2a433f73695da21ff69780d
Alice Birkett <alicebirkett@alice-b-mbp.home>
2023-12-01 13:40:55 +0000

    Completed AoC2015 day 2 part 2

--- src/main/kotlin/katas/AdventOfCode2015Day2.kt
+++ src/main/kotlin/katas/AdventOfCode2015Day2.kt
@@ -2,17 +2,24 @@ package katas
 
 class AdventOfCode2015Day2 {
 
-    fun wrappingPaperCalc(str: String): Int {
-        var counter: Int = 0
+    fun wrappingPaperCalc(str: String): Map<String, Int> {
+        var sum: Int = 0
+        var ribbon: Int = 0
         val splitBoxes: List<String> = str.split("\n")
         splitBoxes.forEach {
             val measurement: List<String> = it.split("x")
-            val intMes: List<Int> = measurement.map {
+            val intList: List<Int> = measurement.map {
                 it.toInt()
             }
-            val small: Int = intMes.sorted()[0] * intMes.sorted()[1]
-            counter += 2 * intMes[0] * intMes[1] + 2 * intMes[1] * intMes[2] +2 * intMes[2] * intMes[0] + small
+            val length: Int = intList[0]
+            val width: Int = intList[1]
+            val height: Int = intList[2]
+            val ordered: List<Int> = intList.sorted()
+            val smallest: Int = ordered[0] * ordered[1]
+            ribbon += (ordered[0] * 2 + ordered[1] * 2) + (length * width * height)
+            sum += (2 * length * width) + (2 * width * height) + (2 * height * length) + smallest
         }
-        return counter
+        return mapOf("paper" to sum, "ribbon" to ribbon)
+
     }
 }
