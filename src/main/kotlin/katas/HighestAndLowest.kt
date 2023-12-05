bc6ce6de4a6c08d0b7da6bb95cb272600564ce5b
Alice Birkett <alicebirkett@alice-b-mbp.home>
2023-12-05 16:19:28 +0000

    adjusted highest and lowest due to bad fold

--- src/main/kotlin/katas/HighestAndLowest.kt
+++ src/main/kotlin/katas/HighestAndLowest.kt
@@ -4,21 +4,12 @@ class HighestAndLowest {
 
     fun highAndLow(numbers: String): String {
 
-        return numbers.split(" ")
+        val sorted: List<Int> = numbers.split(" ")
             .map {
                 it.toInt()
             }
             .sorted()
-            .reversed()
-            .foldIndexed(mutableListOf(0, 0)) {index, acc, num ->
-                if (index == 0) {
-                    acc[0] = num
-                    acc[1] = num
-                } else {
-                    acc[1] = num
-                }
-                acc
-            }
-            .joinToString(" ")
+
+        return "${sorted.last()} ${sorted.first()}"
     }
 }
